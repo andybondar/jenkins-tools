c27678603e2f3f15e4549b6aa9d732c043537fb5
Andriy Bondar <abondar@mirantis.com>
2015-03-17 21:59:45 +0200

    Slight modificatons

--- get_iso_url.py
+++ get_iso_url.py
@@ -3,8 +3,8 @@ import re
 
 # Define Jenkins url and job name
 
-jenkins_url = ''
-jenkins_job = ''
+jenkins_url = 'http://jenkins-product.srt.mirantis.net:8080'
+jenkins_job = 'mox_6.0_iso'
 
 def get_iso_url(url, jobName, username=None, password=None):
     J = Jenkins(url, username, password)
@@ -12,6 +12,5 @@ def get_iso_url(url, jobName, username=None, password=None):
     lgb = job.get_last_good_build()
     return lgb
 
-#print get_iso_url('http://jenkins-product.srt.mirantis.net:8080','mox_6.0_iso').__dict__['_data']['description']
-#print re.findall(r'(http[s]?://\S+)', get_iso_url('http://jenkins-product.srt.mirantis.net:8080','mox_6.0_iso').__dict__['_data']['description'])[0].replace(">ISO","")
+
 print re.findall(r'(http[s]?://\S+)', get_iso_url(jenkins_url,jenkins_job).__dict__['_data']['description'])[0].replace(">ISO","")
