45254d393e9c9aec280ca379d7b6c7c2468b5c34
Andriy Bondar <abondar@mirantis.com>
2015-03-17 21:46:59 +0200

    Added script to get last successfully built ISO url from Jenkins job facts

---
+++ get_iso_url.py
@@ -0,0 +1,17 @@
+from jenkinsapi.jenkins import Jenkins
+import re
+
+# Define Jenkins url and job name
+jenkins_url = ''
+jenkins_job = ''
+
+
+def get_iso_url(url, jobName, username=None, password=None):
+    J = Jenkins(url, username, password)
+    job = J[jobName]
+    lgb = job.get_last_good_build()
+    return lgb
+
+#print get_iso_url('http://jenkins-product.srt.mirantis.net:8080','mox_6.0_iso').__dict__['_data']['description']
+#print re.findall(r'(http[s]?://\S+)', get_iso_url('http://jenkins-product.srt.mirantis.net:8080','mox_6.0_iso').__dict__['_data']['description'])[0].replace(">ISO","")
+print re.findall(r'(http[s]?://\S+)', get_iso_url(jenkins_url,jenkins_job).__dict__['_data']['description'])[0].replace(">ISO","")
