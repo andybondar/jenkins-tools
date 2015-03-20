9a5691b708344cde1e5de8c7e0af834666a4a0b2
Andrii Bondar <abondar@mirantis.com>
2015-03-20 18:49:06 +0200

    Implemented running OSTF tests

--- deploy/tests/ostf.sh
+++ deploy/tests/ostf.sh
@@ -3,9 +3,9 @@
 # $proviant_ip, $proviant_user, $proviant_port $DC_ID to obtain from job parameter
 # $fuel_master_pass, $fuel_master_user, $FUEL_IP  - obtain them from proviant details
 
-echo "Proviant IP: $proviant_ip"
-echo "Proviant user: $proviant_user"
-echo "Proviant port: $proviant_port"
+#echo "Proviant IP: $proviant_ip"
+#echo "Proviant user: $proviant_user"
+#echo "Proviant port: $proviant_port"
 
 # Get FUEL_IP from proviant
 FUEL_IP=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null ${proviant_user}@${proviant_ip} -p ${proviant_port} proviant-dc-details --dc $DC_ID | grep 'Master: hostname:' | awk '{print $7}' | awk -F"://" '{print $2}'`
@@ -14,10 +14,10 @@ fuel_master_pass=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostI
 
 
 
-echo "Fuel IP: $FUEL_IP"
-echo "Fuel logun: $fuel_master_user"
-echo "Fuel pass: $fuel_master_pass"
+#echo "Fuel IP: $FUEL_IP"
+#echo "Fuel logun: $fuel_master_user"
+#echo "Fuel pass: $fuel_master_pass"
 
-#sshpass -p$fuel_master_pass \
-#ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no \
-#$fuel_master_user@$FUEL_IP 'rm -rf /var/log/job-reports; mkdir -p /var/log/job-reports; fuel health --env 1 --check sanity,smoke |tee /var/log/job-reports/ostf.log'
+sshpass -p$fuel_master_pass \
+ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no \
+$fuel_master_user@$FUEL_IP 'rm -rf /var/log/job-reports; mkdir -p /var/log/job-reports; fuel health --env 1 --check sanity,smoke |tee /var/log/job-reports/ostf.log'
