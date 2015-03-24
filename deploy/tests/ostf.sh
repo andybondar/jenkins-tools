f6f7cefabb3f12ed2642fcf2e92e6c3ac5fc4739
Andriy Bondar <abondar@mirantis.com>
2015-03-24 17:29:03 +0200

    Added workaround to wait till OpenStack env deployment is finished. Added analyzing of OSTF checks result.

--- deploy/tests/ostf.sh
+++ deploy/tests/ostf.sh
@@ -8,6 +8,23 @@ FUEL_IP=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUs
 fuel_master_user=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null ${proviant_user}@${proviant_ip} -p ${proviant_port} proviant-dc-details --dc $DC_ID | sed -e '1,/fuel-master-ssh-credentials/d' | head -1 | awk {'print $1'}`
 fuel_master_pass=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null ${proviant_user}@${proviant_ip} -p ${proviant_port} proviant-dc-details --dc $DC_ID | sed -e '1,/fuel-master-ssh-credentials/d' | head -1 | awk {'print $2'}`
 
+# Wait untill OpenStack deployment is done
+count=250
+while [[ $count -ne 0 ]] ; do
+    echo "=== Waiting when OpenStack env is operational.. left $count attempts"
+    status=`ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no $fuel_master_user@$FUEL_IP fuel env --env 1 | tail -1 | awk {'print $3'}`
+    if [ "$status" = "operational" ]; then
+	echo "=== OpenStack env is $status"
+	break
+    fi
+    if [ "$status" = "error" ]; then
+	echo "=== Error! Deployment failed."
+	exit 1
+    fi
+    let count=count-1
+    sleep 60
+done
+
 
 sshpass -p$fuel_master_pass \
 ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no \
@@ -17,3 +34,11 @@ sshpass -p$fuel_master_pass \
 scp -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null -oRSAAuthentication=no -oPubkeyAuthentication=no \
 $fuel_master_user@$FUEL_IP:/var/log/job-reports/ostf.log ostf.log
 
+echo "==============" >> ostf.log
+
+ostf_status=`cat ostf.log | grep failure | wc -l`
+if [ "$ostf_status" -eq "0" ]; then
+    echo "$ostf_status test(s) failed!"
+else
+    echo "All tests passed."
+fi
