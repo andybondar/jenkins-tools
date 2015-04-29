46c19d87733375d5fbb00242a321101aab233232
Andriy Bondar <abondar@mirantis.com>
2015-04-29 15:44:52 +0300

    Added script for correcting symlinks

---
+++ deploy/tests/correct_symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+if [ -z "$BUILD_NUMBER_ISO" ]; then
+    echo "BUILD_NUMBER_ISO is unset, nothing to do with symlinks"
+    exit 0
+fi
+
+if [ "$(tail -n1 ostf.log)" = "All tests passed." ]; then 
+    ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null \
+-oRSAAuthentication=no -p$STORAGE_PORT root@$STORAGE_IP "rm -f /store/fuel_ref/rc; ln -s /store/fuel_ref/$BUILD_NUMBER_ISO /store/fuel_ref/rc; rm -vrf /store/fuel_ref/$PREVIOUS_BUILD"
+else
+    ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null \
+-oRSAAuthentication=no -p$STORAGE_PORT root@$STORAGE_IP "rm -f /store/fuel_ref/test; ln -s /store/fuel_ref/$PREVIOUS_BUILD /store/fuel_ref/test; rm -vrf /store/fuel_ref/$BUILD_NUMBER_ISO"
+fi
