24b63bf22fab7bf040c5dcddda8498acb7182f21
Andriy Bondar <abondar@mirantis.com>
2015-04-29 17:52:21 +0300

    We don't need to delete snapshots here

--- deploy/tests/correct_symlinks.sh
+++ deploy/tests/correct_symlinks.sh
@@ -7,8 +7,8 @@ fi
 
 if [ "$(tail -n1 ostf.log)" = "All tests passed." ]; then 
     ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null \
--oRSAAuthentication=no -p$STORAGE_PORT root@$STORAGE_IP "rm -f /store/fuel_ref/rc; ln -s /store/fuel_ref/$BUILD_NUMBER_ISO /store/fuel_ref/rc; rm -vrf /store/fuel_ref/$PREVIOUS_BUILD"
+-oRSAAuthentication=no -p$STORAGE_PORT root@$STORAGE_IP "rm -f /store/fuel_ref/rc; ln -s /store/fuel_ref/$BUILD_NUMBER_ISO /store/fuel_ref/rc"
 else
     ssh -oConnectTimeout=5 -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null \
--oRSAAuthentication=no -p$STORAGE_PORT root@$STORAGE_IP "rm -f /store/fuel_ref/test; ln -s /store/fuel_ref/$PREVIOUS_BUILD /store/fuel_ref/test; rm -vrf /store/fuel_ref/$BUILD_NUMBER_ISO"
+-oRSAAuthentication=no -p$STORAGE_PORT root@$STORAGE_IP "rm -f /store/fuel_ref/test; ln -s /store/fuel_ref/$PREVIOUS_BUILD /store/fuel_ref/test"
 fi
